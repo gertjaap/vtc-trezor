d013b06d7461ae52c8b6102e52689cd513ce0e0d
Gert-Jaap Glasbergen <gertjaap@gertjaap.nl>
2017-08-12 09:09:55 +0200

    Test code

---
+++ test.js
@@ -0,0 +1,74 @@
+var xpub = "";
+var address = "";
+var vertcoinNetwork =  {
+  messagePrefix: 'Vertcoin Signed Message:\n',
+  bip32: {
+    public: 0x0488b21e,
+    private: 0x0488ade4
+  },
+  pubKeyHash: 0x47,
+  scriptHash: 0x05,
+  wif: 0x80
+};
+
+$('#login').click(function() {
+  TrezorConnect.getXPubKey([44 | 0x80000000, 71 | 0x80000000, 0 | 0x80000000], function (result) {
+    if (result.success) {
+        xpub = result.xpubkey;
+        $('#login').hide();
+        $('#address').show();
+    } else {
+        console.error('Error:', result.error); // error message
+    }
+  });
+});
+
+$('#getAddress').click(function() {
+  var node = bitcoin.HDNode.fromBase58(xpub,vertcoinNetwork);
+  console.log("Node", node);
+  var derived = node.derive(0);
+  console.log("Derived", derived);
+  address = derived.getAddress();
+  console.log("Address", address);
+  $('#address').text(address);
+  $('#spendDiv').show();
+});
+
+$('#spend').click(function() {
+
+  var tx = new bitcoin.TransactionBuilder(vertcoinNetwork);
+  tx.addInput('2de65802ccece97e759058c7dcb4f027ea34437c91f229454e54c961584a7435',1);
+
+  tx.addOutput('VxdYNGYzkcuEpW6cjesdJJtNkkYLKS8By7', 49900000);
+
+  var txRaw = tx.buildIncomplete();
+
+  var signatureHash = txRaw.hashForSignature(0,buffer.Buffer.from("76a9147729097fc7b2f50daa9352429fc8ce8c512b4bd788ac", "hex"),bitcoin.Transaction.SIGHASH_ALL);
+
+  console.log("Hex TX", signatureHash);
+
+
+
+  TrezorConnect.signMessage([44 | 0x80000000, 71 | 0x80000000, 0 | 0x80000000,  0 | 0x80000000,  0 | 0x80000000], ui8arrtoascii(signatureHash), function(result) {
+    if (result.success) {
+      var signature = bitcoin.ECSignature.parseCompact(buffer.Buffer.from(result.signature,'base64'));
+
+      tx.inputs[0].signatures = [signature.signature.toScriptSignature(bitcoin.Transaction.SIGHASH_ALL)];
+      tx.inputs[0].prevOutType = 'pubkey';
+      console.log("Signed TX:",tx.build().toHex());
+
+    } else {
+        console.error('Error:', result.error); // error message
+    }
+  });
+});
+
+function ui8arrtoascii(uintArr)
+{
+  var myString = '';
+  for (var i=0; i<uintArr.byteLength; i++) {
+            myString += String.fromCharCode(uintArr[i])
+        }
+
+        return myString;
+}
